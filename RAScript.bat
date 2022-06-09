9456b56019a6c733b7797839f051740b953ca3ff
TheRhysWyrill <rhyswyrill@gmail.com>
2022-06-09 01:59:21 +0100

    Updated Script

--- RAScript.bat
+++ RAScript.bat
@@ -14,7 +14,8 @@ curl -L https://github.com/TheRhysWyrill/RAScript/raw/main/Required%%20DLLs/DLLs
 curl -L https://github.com/TheRhysWyrill/RAScript/raw/main/Assets/Assets.7z --Output Assets.7z
 curl -L https://github.com/TheRhysWyrill/RAScript/raw/main/Old%%20Cores/Old_Cores.7z --Output Old_Cores.7z
 curl -L https://github.com/GABO1423/Mupen64Plus-Next-XboxOne/releases/latest/download/mupen64plus_next_libretro.7z --Output Mupen64Plus.7z
-curl -L https://github.com/GABO1423/ParaLLEl-N64-XboxOne/releases/latest/download/parallel_n64_libretro.7z --Output ParaLLEl64.7z
+curl -L https://github.com/GABO1423/ParaLLEl-N64-XboxOne/releases/latest/download/parallel_n64_libretro.7z --Output ParLLEl64.7z
+curl -L https://github.com/GABO1423/Flycast-Libretro-Xbox-Builds/releases/latest/download/flycast_libretro.7z --Output Flycast.7z
 
 echo !ESC![105;93m Extracting Everything !ESC![0m
 "C:\Program Files\7-Zip\7z.exe" x RetroArch-Source.zip -o"%~dp0" *.* -r -y
@@ -24,6 +25,7 @@ echo !ESC![105;93m Extracting Everything !ESC![0m
 "C:\Program Files\7-Zip\7z.exe" e Old_Cores.7z -aoa
 "C:\Program Files\7-Zip\7z.exe" e Mupen64Plus.7z -aoa
 "C:\Program Files\7-Zip\7z.exe" e ParaLLEl64.7z -aoa
+"C:\Program Files\7-Zip\7z.exe" e Flycast.7z -aoa
 
 echo !ESC![105;93m Deleting Unwanted Cores !ESC![0m
 del /q /f 81*.dll 2048*.dll a5200*.dll arduous*.dll atari800*.dll bk*.dll blastem*.dll bluemsx*.dll boom3*.dll bsnes*.dll cannonball*.dll cap32*.dll cdi2015*.dll chailove*.dll citra*.dll craft*.dll crocods*.dll desmume_libretro.dll dinothawr*.dll dosbox*.dll ecwolf*.dll fbalpha*.dll fceumm*.dll fixgb*.dll fixnes*.dll fmsx*.dll freechaf*.dll freeintv*.dll frodo*.dll fuse*.dll gearboy*.dll gearcoleco*.dll gearsystem*.dll genesis_plus_gx_wide*.dll gme*.dll gong*.dll gpsp*.dll gw*.dll handy*.dll hatari*.dll jaxe*.dll jumpnbump*.dll kronos*.dll lowresnx*.dll lutro*.dll mame*.dll mednafen_gba*.dll mednafen_lynx*.dll mednafen_pce_fast*.dll mednafen_pce_fast*.dll mednafen_pcfx*.dll mednafen_psx_hw*.dll mednafen_psx*.dll mednafen_snes*.dll mednafen_supafaust*.dll mednafen_supergrafx*.dll mednafen_vb*.dll mesen-s*.dll meteor*.dll minivmac*.dll mrboom*.dll mu_*.dll nekop2*.dll neocd*.dll nestopia*.dll np2kai*.dll nxengine*.dll o2em*.dll oberon*.dll pcsx_rearmed*.dll picodrive*.dll play*.dll pocketcdg*.dll pokemini*.dll potator*.dll prboom*.dll prosystem*.dll puae*.dll px68k*.dll quasi88*.dll quicknes*.dll race*.dll reminiscence*.dll retro8*.dll same_cdi*.dll sameboy*.dll sameduck*.dll scummvm*.dll smsplus*.dll snes9x2*.dll squirreljme*.dll stella*.dll superbroswar*.dll tgbdual*.dll theodore*.dll thepowdertoy*.dll tic80*.dll tyrquake*.dll uzem*.dll vaporspec*.dll vba_next*.dll vbam*.dll vecx*.dll vemulator*.dll vice*.dll virtualjaguar*.dll vitaquake*.dll wasm4*.dll x1*.dll xrick*.dll yabasanshiro*.dll
